fa9431ed29383ee782a41e886520e269702e43a0
Pete Ansell <petea@microsoft.com>
2020-01-14 18:36:16 -0800

    Added some simple translations.

--- Microsoft.Toolkit.Uwp.Input.GazeInteraction/DwellProgressEventArgs.cs
+++ Microsoft.Toolkit.Uwp.Input.GazeInteraction/DwellProgressEventArgs.cs
@@ -1,43 +1,38 @@
-//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
-//See LICENSE in the project root for license information.
+// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
 
-#pragma once
+using System;
+using Windows.UI.Xaml;
 
-#include "DwellProgressState.h"
-
-BEGIN_NAMESPACE_GAZE_INPUT
-
-/// <summary>
-/// This parameter is passed to the GazeElement.DwellProgressFeedback event. The event is fired to inform the application of the user's progress towards completing dwelling on a control
-/// </summary>
-public ref class DwellProgressEventArgs : public RoutedEventArgs
+namespace Microsoft.Toolkit.Uwp.Input.GazeInteraction
 {
-public:
-
     /// <summary>
-    /// An enum that reflects the current state of dwell progress
+    /// This parameter is passed to the GazeElement.DwellProgressFeedback event. The event is fired to inform the application of the user's progress towards completing dwelling on a control
     /// </summary>
-    property DwellProgressState State { DwellProgressState get() { return _state; }}
+    public class DwellProgressEventArgs : RoutedEventArgs
+    {
+        /// <summary>
+        /// An enum that reflects the current state of dwell progress
+        /// </summary>
+        public DwellProgressState State => _state;
 
-    /// <summary>
-    /// A value between 0 and 1 that reflects the fraction of progress towards completing dwell
-    /// </summary>
-    property double Progress { double get() { return _progress; }}
+        /// <summary>
+        /// A value between 0 and 1 that reflects the fraction of progress towards completing dwell
+        /// </summary>
+        public double Progress => _progress;
 
-    /// <summary>
-    /// A parameter for the application to set to true if it handles the event. If this parameter is set to true, the library suppresses default animation for dwell feedback on the control
-    /// </summary>
-    property bool Handled;
+        /// <summary>
+        /// A parameter for the application to set to true if it handles the event. If this parameter is set to true, the library suppresses default animation for dwell feedback on the control
+        /// </summary>
+        public bool Handled { get; set; }
 
-internal:
-	DwellProgressEventArgs(DwellProgressState state, TimeSpan elapsedDuration, TimeSpan triggerDuration)
-	{
-        _state = state;
-        _progress = ((double)elapsedDuration.Duration) / triggerDuration.Duration;
-	}
-private:
-    DwellProgressState _state;
-    double _progress;
-};
+        internal DwellProgressEventArgs(DwellProgressState state, TimeSpan elapsedDuration, TimeSpan triggerDuration)
+        {
+            _state = state;
+            _progress = ((double)elapsedDuration.Ticks) / triggerDuration.Ticks;
+        }
 
-END_NAMESPACE_GAZE_INPUT
+        private DwellProgressState _state;
+        private double _progress;
+    }
+}
