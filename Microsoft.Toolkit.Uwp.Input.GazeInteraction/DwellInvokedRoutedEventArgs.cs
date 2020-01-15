f699e9ebb9b2813c6caf79f32b5585efddd56c02
Pete Ansell <petea@microsoft.com>
2020-01-14 18:27:11 -0800

    Added first first C# converted source

--- Microsoft.Toolkit.Uwp.Input.GazeInteraction/DwellInvokedRoutedEventArgs.cs
+++ Microsoft.Toolkit.Uwp.Input.GazeInteraction/DwellInvokedRoutedEventArgs.cs
@@ -1,28 +1,23 @@
-//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
-//See LICENSE in the project root for license information.
+// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
 
-#pragma once
+using Windows.UI.Xaml;
 
-BEGIN_NAMESPACE_GAZE_INPUT
-
-/// <summary>
-/// This parameter is passed to the GazeElement::Invoked event and allows 
-/// the application to prevent default invocation when the user dwells on a control
-/// </summary>
-public ref class DwellInvokedRoutedEventArgs : public RoutedEventArgs
+namespace Microsoft.Toolkit.Uwp.Input.GazeInteraction
 {
-public:
-
     /// <summary>
-    /// The application should set this value to true to prevent invoking the control when the user dwells on a control
+    /// This parameter is passed to the GazeElement::Invoked event and allows
+    /// the application to prevent default invocation when the user dwells on a control
     /// </summary>
-    property bool Handled;
-
-internal:
-
-    DwellInvokedRoutedEventArgs()
+    public class DwellInvokedRoutedEventArgs : RoutedEventArgs
     {
-    }
-};
+        /// <summary>
+        /// Gets or sets a value indicating whether the application should set this value to true to prevent invoking the control when the user dwells on a control
+        /// </summary>
+        public bool Handled { get; set; }
 
-END_NAMESPACE_GAZE_INPUT
+        internal DwellInvokedRoutedEventArgs()
+        {
+        }
+    }
+}
