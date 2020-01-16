f93ca1ecf4a5a19c6e09b5b68139283bc103217c
Pete Ansell <petea@microsoft.com>
2020-01-15 20:26:34 -0800

    Converted GazeEventArgs

--- Microsoft.Toolkit.Uwp.Input.GazeInteraction/GazeEventArgs.cs
+++ Microsoft.Toolkit.Uwp.Input.GazeInteraction/GazeEventArgs.cs
@@ -1,5 +1,24 @@
-//Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
-//See LICENSE in the project root for license information.
+// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
 
-#include "pch.h"
-#include "GazeEventArgs.h"
+using System;
+using Windows.Foundation;
+
+namespace Microsoft.Toolkit.Uwp.Input.GazeInteraction
+{
+    public sealed class GazeEventArgs
+    {
+        public bool Handled { get; set; }
+
+        public Point Location { get; set; }
+
+        public TimeSpan Timestamp { get; set; }
+
+        public void Set(Point location, TimeSpan timestamp)
+        {
+            Handled = false;
+            Location = location;
+            Timestamp = timestamp;
+        }
+    }
+}
