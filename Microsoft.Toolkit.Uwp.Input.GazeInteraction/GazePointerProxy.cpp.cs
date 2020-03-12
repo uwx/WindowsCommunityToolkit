a0604daa62ea89ade92f3dbbf749d9707948bb88
Pete Ansell <petea@microsoft.com>
2020-03-11 21:36:36 -0700

    More conversion.

--- Microsoft.Toolkit.Uwp.Input.GazeInteraction/GazePointerProxy.cpp.cs
+++ Microsoft.Toolkit.Uwp.Input.GazeInteraction/GazePointerProxy.cpp.cs
@@ -1,135 +1,173 @@
 //Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
 //See LICENSE in the project root for license information.
 
-namespace Microsoft.Toolkit.Uwp.Input.GazeInteraction { /*
+using System;
+using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
 
-/// <summary>
-/// The IsLoaded heuristic for testing whether a FrameworkElement is in the visual tree.
-/// </summary>
-static bool IsLoadedHeuristic(FrameworkElement element)
+namespace Microsoft.Toolkit.Uwp.Input.GazeInteraction
 {
-    bool isLoaded;
-
-    // element.Loaded has already happened if it is in the visual tree...
-    var parent = VisualTreeHelper.GetParent(element);
-    if (parent != null)
+    /// <summary>
+    /// Helper class that helps track which UIElements in the visual tree are enabled.
+    ///
+    /// The GazePointer is enabled when one or more UIElements in the visual tree have
+    /// their GazeInput.InteractionProperty value set to Enabled. Notice that there are
+    /// two conditions for enablement: that attached property is Enabled; that the UIElement
+    /// is in the visual tree.
+    /// </summary>
+    internal sealed partial class GazePointerProxy
     {
-        isLoaded = true;
-    }
-    // ...or...
-    else
-    {
-        // ...if the element is a dynamically created Popup that has been opened.
-        var popup = dynamic_cast<Popup>(element);
-        isLoaded = popup != null && popup.IsOpen;
-    }
 
-    return isLoaded;
-}
-
-DependencyProperty GazePointerProxy.GazePointerProxyProperty.get()
-{
-    // The attached property registration.
-    static var value = DependencyProperty.RegisterAttached("_GazePointerProxy", GazePointerProxy.typeid, GazePointerProxy.typeid,
-        new PropertyMetadata(null));
-    return value;
-}
-
-GazePointerProxy.GazePointerProxy(FrameworkElement element)
-{
-    static int lastId = 0;
-    lastId++;
-    _uniqueId = lastId;
-
-    _isLoaded = IsLoadedHeuristic(element);
-
-    // Start watching for the element to enter and leave the visual tree.
-    element.Loaded += new RoutedEventHandler(this, &GazePointerProxy.OnLoaded);
-    element.Unloaded += new RoutedEventHandler(this, &GazePointerProxy.OnUnloaded);
-}
-
-void GazePointerProxy.SetInteraction(FrameworkElement element, Interaction value)
-{
-    // Get or create a GazePointerProxy for element.
-    var proxy = safe_cast<GazePointerProxy>(element.GetValue(GazePointerProxyProperty));
-    if (proxy == null)
-    {
-        proxy = new GazePointerProxy(element);
-        element.SetValue(GazePointerProxyProperty, proxy);
-    }
-
-    // Set the proxy's _isEnabled value.
-    proxy.SetIsEnabled(element, value == Interaction.Enabled);
-}
-
-void GazePointerProxy.SetIsEnabled(Object sender, bool value)
-{
-    // If we have a new value...
-    if (_isEnabled != value)
-    {
-        // ...record the new value.
-        _isEnabled = value;
-
-        // If we are in the visual tree...
-        if (_isLoaded)
+        /// <summary>
+        /// The IsLoaded heuristic for testing whether a FrameworkElement is in the visual tree.
+        /// </summary>
+        static bool IsLoadedHeuristic(FrameworkElement element)
         {
-            // ...if we're being enabled...
-            if (value)
+            bool isLoaded;
+
+            // element.Loaded has already happened if it is in the visual tree...
+            var parent = VisualTreeHelper.GetParent(element);
+            if (parent != null)
             {
-                // ...count the element in...
-                GazePointer.Instance.AddRoot(_uniqueId);
+                isLoaded = true;
+            }
+            // ...or...
+            else
+            {
+                // ...if the element is a dynamically created Popup that has been opened.
+                var popup = element as Popup;
+                isLoaded = popup != null && popup.IsOpen;
+            }
+
+            return isLoaded;
+        }
+
+        /// <summary>
+        /// A private attached property for associating an instance of this class with the UIElement
+        /// to which it refers.
+        /// </summary>
+        public static DependencyProperty GazePointerProxyProperty { get; } =
+            DependencyProperty.RegisterAttached("_GazePointerProxy", typeof(GazePointerProxy), typeof(GazePointerProxy),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="element">The element proxy is attaching to.</param>
+        private GazePointerProxy(FrameworkElement element)
+        {
+            lastId++;
+            _uniqueId = lastId;
+
+            _isLoaded = IsLoadedHeuristic(element);
+
+            // Start watching for the element to enter and leave the visual tree.
+            element.Loaded += OnLoaded;
+            element.Unloaded += OnUnloaded;
+        }
+        static int lastId = 0;
+
+        /// <summary>
+        /// Method called when the GazeInput.Interaction attached property is set to a new value.
+        /// </summary>
+        /// <param name="element">The element being set. May be null to indicate whole user interface.</param>
+        /// <param name="value">The interaction enablement value being set.</param>
+        internal static void SetInteraction(FrameworkElement element, Interaction value)
+        {
+            // Get or create a GazePointerProxy for element.
+            var proxy = (GazePointerProxy)element.GetValue(GazePointerProxyProperty);
+            if (proxy == null)
+            {
+                proxy = new GazePointerProxy(element);
+                element.SetValue(GazePointerProxyProperty, proxy);
+            }
+
+            // Set the proxy's _isEnabled value.
+            proxy.SetIsEnabled(element, value == Interaction.Enabled);
+        }
+
+        /// <summary>
+        /// Set the enablement of this proxy.
+        /// </summary>
+        /// <param name="sender">The object setting the enable value.</param>
+        /// <param name="value">The new enable value.</param>
+        private void SetIsEnabled(Object sender, bool value)
+        {
+            // If we have a new value...
+            if (_isEnabled != value)
+            {
+                // ...record the new value.
+                _isEnabled = value;
+
+                // If we are in the visual tree...
+                if (_isLoaded)
+                {
+                    // ...if we're being enabled...
+                    if (value)
+                    {
+                        // ...count the element in...
+                        GazePointer.Instance.AddRoot(_uniqueId);
+                    }
+                    else
+                    {
+                        // ...otherwise count the element out.
+                        GazePointer.Instance.RemoveRoot(_uniqueId);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// The handler to be called when the corresponding element joins the visual tree.
+        /// </summary>
+        private void OnLoaded(Object sender, RoutedEventArgs args)
+        {
+            Debug.Assert(IsLoadedHeuristic((FrameworkElement)sender));
+
+            if (!_isLoaded)
+            {
+                // Record that we are now loaded.
+                _isLoaded = true;
+
+                // If we were previously enabled...
+                if (_isEnabled)
+                {
+                    // ...we can now be counted as actively enabled.
+                    GazePointer.Instance.AddRoot(_uniqueId);
+                }
             }
             else
             {
-                // ...otherwise count the element out.
-                GazePointer.Instance.RemoveRoot(_uniqueId);
+                Debug.WriteLine("Unexpected Load");
             }
         }
-    }
-}
 
-void GazePointerProxy.OnLoaded(Object sender, RoutedEventArgs args)
-{
-    assert(IsLoadedHeuristic(safe_cast<FrameworkElement>(sender)));
-
-    if (!_isLoaded)
-    {
-        // Record that we are now loaded.
-        _isLoaded = true;
-
-        // If we were previously enabled...
-        if (_isEnabled)
+        /// <summary>
+        /// The handler to be called when the corresponding element leaves the visual tree.
+        /// </summary>
+        private void OnUnloaded(Object sender, RoutedEventArgs args)
         {
-            // ...we can now be counted as actively enabled.
-            GazePointer.Instance.AddRoot(_uniqueId);
+            Debug.Assert(!IsLoadedHeuristic((FrameworkElement)sender));
+
+            if (_isLoaded)
+            {
+                // Record that we have left the visual tree.
+                _isLoaded = false;
+
+                // If we are set as enabled...
+                if (_isEnabled)
+                {
+                    // ...we no longer count as being actively enabled (because we have fallen out the visual tree).
+                    GazePointer.Instance.RemoveRoot(_uniqueId);
+                }
+            }
+            else
+            {
+                Debug.WriteLine("Unexpected unload");
+            }
         }
-    }
-    else
-    {
-        Debug.WriteLine(L"Unexpected Load");
+
     }
 }
-
-void GazePointerProxy.OnUnloaded(Object sender, RoutedEventArgs args)
-{
-    assert(!IsLoadedHeuristic(safe_cast<FrameworkElement>(sender)));
-
-    if (_isLoaded)
-    {
-        // Record that we have left the visual tree.
-        _isLoaded = false;
-
-        // If we are set as enabled...
-        if (_isEnabled)
-        {
-            // ...we no longer count as being actively enabled (because we have fallen out the visual tree).
-            GazePointer.Instance.RemoveRoot(_uniqueId);
-        }
-    }
-    else
-    {
-        Debug.WriteLine(L"Unexpected unload");
-    }
-}
-
-*/ }
--- Microsoft.Toolkit.Uwp.Input.GazeInteraction/GazePointerProxy.h.cs
+++ Microsoft.Toolkit.Uwp.Input.GazeInteraction/GazePointerProxy.h.cs
@@ -6,7 +6,6 @@ using Windows.UI.Xaml;
 
 namespace Microsoft.Toolkit.Uwp.Input.GazeInteraction
 {
-
     /// <summary>
     /// Helper class that helps track which UIElements in the visual tree are enabled.
     ///
@@ -15,47 +14,8 @@ namespace Microsoft.Toolkit.Uwp.Input.GazeInteraction
     /// two conditions for enablement: that attached property is Enabled; that the UIElement
     /// is in the visual tree.
     /// </summary>
-    internal sealed class GazePointerProxy
+    internal sealed partial class GazePointerProxy
     {
-        /// <summary>
-        /// A private attached property for associating an instance of this class with the UIElement
-        /// to which it refers.
-        /// </summary>
-        public static DependencyProperty GazePointerProxyProperty
-        {
-            get { throw new ToDoException(); }
-        }
-
-    /// <summary>
-    /// Method called when the GazeInput.Interaction attached property is set to a new value.
-    /// </summary>
-    /// <param name="element">The element being set. May be null to indicate whole user interface.</param>
-    /// <param name="value">The interaction enablement value being set.</param>
-        internal static void SetInteraction(FrameworkElement element, Interaction value) { throw new ToDoException(); }
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        /// <param name="element">The element proxy is attaching to.</param>
-        private GazePointerProxy(FrameworkElement element) { throw new ToDoException(); }
-
-        /// <summary>
-        /// Set the enablement of this proxy.
-        /// </summary>
-        /// <param name="sender">The object setting the enable value.</param>
-        /// <param name="value">The new enable value.</param>
-        private void SetIsEnabled(Object sender, bool value) { throw new ToDoException(); }
-
-        /// <summary>
-        /// The handler to be called when the corresponding element joins the visual tree.
-        /// </summary>
-        private void OnLoaded(Object sender, RoutedEventArgs args) { throw new ToDoException(); }
-
-        /// <summary>
-        /// The handler to be called when the corresponding element leaves the visual tree.
-        /// </summary>
-        private void OnUnloaded(Object sender, RoutedEventArgs args) { throw new ToDoException(); }
-
         /// <summary>
         /// Non-zero ID associated with this instance.
         /// </summary>
